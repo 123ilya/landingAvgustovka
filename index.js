40b682ba292c730a9c7eee3360943fc5fdb472e8
ilya <123ilya@gmail.com>
2022-08-09 00:05:45 +1000

    закомментировал js

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-let button22 = document.querySelector("#scheldule_button_22");
-let button23 = document.querySelector("#scheldule_button_23");
-let button24 = document.querySelector("#scheldule_button_24");
+// let button22 = document.querySelector("#scheldule_button_22");
+// let button23 = document.querySelector("#scheldule_button_23");
+// let button24 = document.querySelector("#scheldule_button_24");
 
-let scheldule22AG = document.querySelector("#scheldule_22AG");
-let scheldule23AG = document.querySelector("#scheldule_23AG");
-let scheldule24AG = document.querySelector("#scheldule_24AG");
+// let scheldule22AG = document.querySelector("#scheldule_22AG");
+// let scheldule23AG = document.querySelector("#scheldule_23AG");
+// let scheldule24AG = document.querySelector("#scheldule_24AG");
 
-button22.addEventListener("click", button22Func);
-function button22Func() {
-  scheldule22AG.classList.add("notdisplay");
-  console.log("fff");
-}
+// button22.addEventListener("click", button22Func);
+// function button22Func() {
+//   scheldule22AG.classList.add("notdisplay");
+// }

2fcb2a765ed772d77315d58a58b61f4156b5e74e
Илья А. Соболев <sobolev_ia@pippkro.ru>
2022-08-11 18:01:52 +1000

    добавил переключение вкладок расписания, якоря тупят

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
-// let button22 = document.querySelector("#scheldule_button_22");
-// let button23 = document.querySelector("#scheldule_button_23");
-// let button24 = document.querySelector("#scheldule_button_24");
+let button22 = document.querySelector("#button22");
+let button23 = document.querySelector("#button23");
+let button24 = document.querySelector("#button24");
 
-// let scheldule22AG = document.querySelector("#scheldule_22AG");
-// let scheldule23AG = document.querySelector("#scheldule_23AG");
-// let scheldule24AG = document.querySelector("#scheldule_24AG");
+button22.addEventListener("click", event22);
+function event22() {
+  document.querySelector("#scheldule_22AG").classList.remove("displaynone");
+  document.querySelector("#scheldule_23AG").classList.add("displaynone");
+  document.querySelector("#scheldule_24AG").classList.add("displaynone");
+  button22.classList.add("pressed");
+  button23.classList.remove("pressed");
+  button24.classList.remove("pressed");
+  
+}
 
-// button22.addEventListener("click", button22Func);
-// function button22Func() {
-//   scheldule22AG.classList.add("notdisplay");
-// }
+button23.addEventListener("click", event23);
+function event23() {
+  document.querySelector("#scheldule_22AG").classList.add("displaynone");
+  document.querySelector("#scheldule_23AG").classList.remove("displaynone");
+  document.querySelector("#scheldule_24AG").classList.add("displaynone");
+  button22.classList.remove("pressed");
+  button23.classList.add("pressed");
+  button24.classList.remove("pressed");
+  
+}
+
+button24.addEventListener("click", event24);
+function event24() {
+  document.querySelector("#scheldule_22AG").classList.add("displaynone");
+  document.querySelector("#scheldule_23AG").classList.add("displaynone");
+  document.querySelector("#scheldule_24AG").classList.remove("displaynone");
+  button22.classList.remove("pressed");
+  button23.classList.remove("pressed");
+  button24.classList.add("pressed");
+  
+}
